c382fa4cf07877ff8ab2c64b8dc3972e780430d7
Mehedi Hasan Shahed <mehedihasanshahed3@gmail.com>
2026-02-12 22:02:45 +0600

    added booking consultation component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import BookingConsultation from './Components/BookingConsultation/BookingConsultation';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/sign-up" element={<Sign_Up />} />
           <Route path="/login" element={<Login />} />
           <Route path="/instant-consultation" element={<InstantConsultation />} />
+          <Route path="/booking-consultation" element={<BookingConsultation />} />
         </Routes>
       </BrowserRouter>
     </>
