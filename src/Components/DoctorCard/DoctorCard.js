9b2b2e54ce3ca31f824148e00ca93c736b3d86f0
Mehedi Hasan Shahed <mehedihasanshahed3@gmail.com>
2026-02-12 21:58:24 +0600

    added booking and cancellation

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -45,67 +45,67 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
                     <div className="doctor-card-detail-consultationfees">Ratings: {ratings}</div>
                 </div>
 
-                {/* <div className="doctor-card-options-container"> */}
-                <Popup
-                    trigger={
-                        <button className={`book-appointment-btn ${appointments.length > 0 ? 'cancel-appointment' : ''}`}>
-                            {appointments.length > 0 ? (
-                                <div>Cancel Appointment</div>
-                            ) : (
-                                <div>Book Appointment</div>
-                            )}
-                            <div>No Booking Fee</div>
-                        </button>
-                    }
-                    modal
-                    open={showModal}
-                    onClose={() => setShowModal(false)}
-                    contentStyle={{
-                        width: '500px',
-                        background: '#fff',
-                        borderRadius: '20px',
-                        padding: '0',
-                        border: 'none',
-                        boxShadow: '0 10px 30px rgba(0,0,0,0.2)',
-                        zIndex: '2000'
-                    }}
-                    overlayStyle={{
-                        background: 'rgba(0,0,0,0.5)',
-                        zIndex: '1999'
-                    }}
-                >
-                    {(close) => (
-                        <div className="doctorbg" style={{ overflow: 'auto', padding: '20px' }}>
-                            <div>
-                                <div className="doctor-card-profile-image-container">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" /> </svg>
+                <div className="doctor-card-options-container">
+                    <Popup
+                        trigger={
+                            <button className={`book-appointment-btn ${appointments.length > 0 ? 'cancel-appointment' : ''}`}>
+                                {appointments.length > 0 ? (
+                                    <div>Cancel Appointment</div>
+                                ) : (
+                                    <div>Book Appointment</div>
+                                )}
+                                <div>No Booking Fee</div>
+                            </button>
+                        }
+                        modal
+                        open={showModal}
+                        onClose={() => setShowModal(false)}
+                        contentStyle={{
+                            width: '500px',
+                            background: '#fff',
+                            borderRadius: '20px',
+                            padding: '0',
+                            border: 'none',
+                            boxShadow: '0 10px 30px rgba(0,0,0,0.2)',
+                            zIndex: '2000'
+                        }}
+                        overlayStyle={{
+                            background: 'rgba(0,0,0,0.5)',
+                            zIndex: '1999'
+                        }}
+                    >
+                        {(close) => (
+                            <div className="doctorbg" style={{ overflow: 'auto', padding: '20px' }}>
+                                <div>
+                                    <div className="doctor-card-profile-image-container">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" /> </svg>
+                                    </div>
+                                    <div className="doctor-card-details">
+                                        <div className="doctor-card-detail-name">{name}</div>
+                                        <div className="doctor-card-detail-speciality">{speciality}</div>
+                                        <div className="doctor-card-detail-experience">{experience} years experience</div>
+                                        <div className="doctor-card-detail-consultationfees">Ratings: {ratings}</div>
+                                    </div>
                                 </div>
-                                <div className="doctor-card-details">
-                                    <div className="doctor-card-detail-name">{name}</div>
-                                    <div className="doctor-card-detail-speciality">{speciality}</div>
-                                    <div className="doctor-card-detail-experience">{experience} years experience</div>
-                                    <div className="doctor-card-detail-consultationfees">Ratings: {ratings}</div>
-                                </div>
-                            </div>
 
-                            {appointments.length > 0 ? (
-                                <>
-                                    <h3 style={{ textAlign: 'center' }}>Appointment Booked!</h3>
-                                    {appointments.map((appointment) => (
-                                        <div className="bookedInfo" key={appointment.id}>
-                                            <p>Name: {appointment.name}</p>
-                                            <p>Phone Number: {appointment.phoneNumber}</p>
-                                            <button onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
-                                        </div>
-                                    ))}
-                                </>
-                            ) : (
-                                <AppointmentForm doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
-                            )}
-                        </div>
-                    )}
-                </Popup>
-                {/* </div> */}
+                                {appointments.length > 0 ? (
+                                    <>
+                                        <h3 style={{ textAlign: 'center' }}>Appointment Booked!</h3>
+                                        {appointments.map((appointment) => (
+                                            <div className="bookedInfo" key={appointment.id}>
+                                                <p>Name: {appointment.name}</p>
+                                                <p>Phone Number: {appointment.phoneNumber}</p>
+                                                <button onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
+                                            </div>
+                                        ))}
+                                    </>
+                                ) : (
+                                    <AppointmentForm doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
+                                )}
+                            </div>
+                        )}
+                    </Popup>
+                </div>
             </div>
         </div>
     );
